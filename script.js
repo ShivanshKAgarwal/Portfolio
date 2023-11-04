fc5705851a9eb186629b8af97992c2b2d48c5db0
Shivansh Agarwal <bindals1803@gmail.com>
2023-11-04 12:06:30 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  function showContactDetails() {
+    if (confirm("Are you sure you want to view the contact information?")) {
+      window.open('https://wa.me/9719070084?text=Hi,%20I%20would%20like%20to%20discuss%20a%20Project%20with%20you.', '_blank');
+    }
+}
+
+function scrollToExperience() {
+    const experienceSection = document.getElementById('experience');
+    if (experienceSection) {
+        experienceSection.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+function scrollToProjects() {
+  const projectsSection = document.getElementById('projects');
+  if (projectsSection) {
+      projectsSection.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+function scrollToContacts() {
+  const contactsSection = document.getElementById('contact');
+  if (contactsSection) {
+      contactsSection.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
